408e2451d5472561984a2c0a2cb841d3c17e0f21
patilMohitDD <70277626+patilMohitDD@users.noreply.github.com>
2023-07-17 18:13:23 -0700

    added more Booking objectsn

--- src/main/java/com/showstopper/booking/model/BookingData.java
+++ src/main/java/com/showstopper/booking/model/BookingData.java
@@ -10,9 +10,15 @@ public class BookingData {
     public BookingData(){
         Booking firstBooking = new Booking(10, "AB-10", "MI:6", " 3pm", "6pm");
         Booking secondBooking = new Booking(12, "AB-11", "MI:7", " 9am", "12pm");
+        Booking thirdBooking = new Booking(14, "AB-12", "MI:8", " 10am", "1pm");
+        Booking fourthBooking = new Booking(16, "AB-13", "MI:9", " 11am", "2pm");
+        Booking fifthBooking = new Booking(18, "AB-14", "MI:10", " 11am", "1pm");
 
         bookings.add(firstBooking);
         bookings.add(secondBooking);
+        bookings.add(thirdBooking);
+        bookings.add(fourthBooking);
+        bookings.add(fifthBooking);
     }
 
     public ArrayList<Booking>  getALLBookings(){
@@ -23,7 +29,6 @@ public class BookingData {
         Optional <Booking> requiredBooking = this.bookings.stream()
                 .filter(booking-> booking.get_id() == id)
                 .findFirst();
-
         return requiredBooking;
 
         }
