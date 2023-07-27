659ddd1c865474b289ef8a9738670b7afcdc3165
patilMohitDD <70277626+patilMohitDD@users.noreply.github.com>
2023-07-26 18:53:32 -0700

    Create getTheatreInfo endpoint in Theatre controller

--- src/main/java/com/showstopper/booking/controller/TheatreController.java
+++ src/main/java/com/showstopper/booking/controller/TheatreController.java
@@ -1,5 +1,6 @@
 package com.showstopper.booking.controller;
 
+import com.showstopper.booking.model.Booking;
 import com.showstopper.booking.model.Theatre;
 import com.showstopper.booking.service.TheatreService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +12,27 @@ import java.util.List;
 @RestController
 @RequestMapping("theatre")
 public class TheatreController {
-
     @Autowired
     private TheatreService theatreService;
+    @GetMapping("/")
+    public ResponseEntity<List<Theatre>> getAllTheatre() {
+        return ResponseEntity.ok(theatreService.getAll());
+    }
 
     @PostMapping("/create")
     public ResponseEntity<Integer> createThreatre(@RequestBody Theatre theatre){
         int createdTheatre = theatreService.addTheatre(theatre);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTheatre);
     }
-    @GetMapping("/")
-    public ResponseEntity<List<Theatre>> getAllTheatre() {
-        return ResponseEntity.ok(theatreService.getAll());
+    @GetMapping("/info/{id}")
+    public ResponseEntity<Theatre> getTheatreInfo(@PathVariable int id) throws Exception {
+        Theatre getTheatreInfo = theatreService.getTheatreInfo(id);
+
+        if (getTheatreInfo != null) {
+            return ResponseEntity.ok(getTheatreInfo);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/com/showstopper/booking/service/TheatreService.java
+++ src/main/java/com/showstopper/booking/service/TheatreService.java
@@ -1,11 +1,13 @@
 package com.showstopper.booking.service;
 
+import com.showstopper.booking.model.Booking;
 import com.showstopper.booking.model.Theatre;
 import com.showstopper.booking.repository.TheatreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TheatreService {
@@ -15,8 +17,17 @@ public class TheatreService {
         Theatre theatre = theatreRepository.save(newTheatre);
         return theatre.getId();
     }
-
     public List<Theatre> getAll() {
         return theatreRepository.findAll();
+    }
+
+    public Theatre getTheatreInfo(int id) throws Exception {
+        Optional<Theatre> requiredTheatreInfo = theatreRepository.findById(id);
+
+        if ( requiredTheatreInfo.isPresent() ){
+            Theatre existingTheatreInfo  = requiredTheatreInfo.get();
+            return existingTheatreInfo;
+        }
+        throw new Exception("ID not Found");
     }
 }
